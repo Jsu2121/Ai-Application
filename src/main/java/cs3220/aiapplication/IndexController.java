bbe89cb9798d65edec0bbc634e6412c4c1b3cef0
Mohammad <baboradib675@gmail.com>
2025-10-30 13:54:50 -0700

    Added controller class

---
+++ src/main/java/cs3220/aiapplication/IndexController.java
@@ -0,0 +1,13 @@
+package cs3220.aiapplication;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
